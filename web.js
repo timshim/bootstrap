05340786fc23940683664062e9cfc60cf9fee764
Tim Shim <timshim@gmail.com>
2013-09-07 11:47:14 +0000

    Added web.js and Procfile

---
+++ web.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var data = fs.readFileSync("index.html", "utf-8");
+
+app.get('/', function(request, response) {
+  response.send(data);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
