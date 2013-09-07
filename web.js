ad6b93a2a6a5175eadd6a960143695ed1fe4b7ab
Tim Shim <timshim@gmail.com>
2013-09-07 12:20:47 +0000

    Installed express

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
