7e0959d7b924ac08bf831be39d0dcdb410367a19
Tim Shim <timshim@gmail.com>
2013-09-09 04:20:07 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,5 +1,8 @@
-var express = require("express");
-var app = express();
+var fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 var data = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
