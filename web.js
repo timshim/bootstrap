db18ee32bedcf8aa8cbc26da63c7ee4c78dc18bc
Tim Shim <timshim@gmail.com>
2013-09-12 07:53:56 +0000

    Edited web.js and index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-app.use(express.static(__dirname + '/assets/css'));
+app.configure(function(){app.use(express.static(__dirname + '/assets'));});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
