931b0e2144760fc971384563cd0c8b45decc158c
Tim Shim <timshim@gmail.com>
2013-09-12 07:47:29 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/assets/css'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
